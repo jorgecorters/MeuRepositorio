6132362d9180e7d9ed0cccd58369b2abada4a92f
Jorge Leonardo <eonardojorge201@gmail.com>
2026-08-01 13:13:59 -0300

    Adicionando diretorio de CSharp

---
+++ RtornoC#/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Globalization;
+
+string nome;
+int quartos;
+double preco;
+
+
+Console.WriteLine("Entre com seu nome completo: ");
+nome = Console.ReadLine();
+Console.WriteLine("Quantos quartos tem na sua casa? ");
+quartos = int.Parse(Console.ReadLine());
+Console.WriteLine("Entre com o preco do produto: ");
+preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.WriteLine("Entre com seu ultimo nome, idade e altura(mesma linha)");
+
+string[] vet = Console.ReadLine().Split(' ');
+string nome2 = vet[0];
+int idade = int.Parse(vet[1]);
+double altura = double.Parse(vet[2]);
+
+Console.WriteLine(nome);
+Console.WriteLine(quartos);
+Console.WriteLine(preco.ToString("F2"), CultureInfo.InvariantCulture);
+Console.WriteLine(nome2);
+Console.WriteLine(idade);
+Console.WriteLine(altura.ToString("F2"), CultureInfo.InvariantCulture);
+
+
+
+
+
