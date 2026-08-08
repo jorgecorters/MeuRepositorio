814571fde352f36a1bdba5e83ea1507e979da6cf
Jorge Leonardo <eonardojorge201@gmail.com>
2026-08-08 12:29:05 -0300

    Adicionando mais exercicios em Csharp

---
+++ Exercicios em Csharp/Exercicio3/Funcionario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio3
+{
+    internal class Funcionario
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+        public void AumentarSalario(double porcentagem)
+        {
+            SalarioBruto = SalarioBruto + (SalarioBruto *  porcentagem / 100.0); 
+        }
+
+       
+    }
+}
---
+++ Exercicios em Csharp/Exercicio3/Program.cs
@@ -0,0 +1,22 @@
+﻿using Exercicio3;
+
+Funcionario funcionario1 = new Funcionario();
+
+
+
+Console.WriteLine("Digite o nome, salario bruto e imposto do funcionario: ");
+funcionario1.Nome = Console.ReadLine();
+funcionario1.SalarioBruto = double.Parse(Console.ReadLine());
+funcionario1.Imposto = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Funcionario: " + funcionario1.Nome + ", $ " + funcionario1.SalarioLiquido().ToString("F3"));
+Console.WriteLine();
+
+Console.WriteLine("Digite a porcentagem para aumentar o salario: ");
+double porcent = double.Parse(Console.ReadLine());
+funcionario1.AumentarSalario(porcent);
+
+Console.WriteLine();
+Console.WriteLine("DADOS ATUALIZADOS: " + funcionario1.Nome + ", $ " + funcionario1.SalarioBruto.ToString("F3"));
+
+
