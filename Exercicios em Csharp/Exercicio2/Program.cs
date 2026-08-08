033fd6ba2b4184f731aa196d1cbcec2c63679b1c
Jorge Leonardo <eonardojorge201@gmail.com>
2026-08-08 11:32:14 -0300

    Adicionando novo exercicio em Csharp

---
+++ Exercicios em Csharp/Exercicio2/Program.cs
@@ -0,0 +1,13 @@
+﻿using Exercicio2;
+
+Retangulo ret = new Retangulo();
+
+Console.WriteLine("Entre com altura e largura do objeto: ");
+ret.Largura = double.Parse(Console.ReadLine());
+ret.Altura  = double.Parse(Console.ReadLine());
+
+Console.WriteLine("AREA = " + ret.Area().ToString("F2"));
+Console.WriteLine("PERIMETRO = " + ret.Perimetro().ToString("F2"));
+Console.WriteLine("DIAGONAL = " + ret.Diagonal().ToString("F2"));
+
+
---
+++ Exercicios em Csharp/Exercicio2/Retangulo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio2
+
+{
+    internal class Retangulo
+    {
+        public double Altura;
+        public double Largura;
+
+        public double Area()
+        {
+            return Altura * Largura;        }
+        public double Perimetro()
+        {
+            return 2 * (Altura * Largura);
+        }
+        public double Diagonal()
+        {
+            return Math.Sqrt(Largura * Largura + Altura * Altura);
+        }
+    }
+}
